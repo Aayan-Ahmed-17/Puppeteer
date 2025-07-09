df8f00f6caa510a3d8e674660270a488c8a15973
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-07-09 13:56:10 +0500

    code enhance and readable dn

--- scrape.js
+++ scrape.js
@@ -8,16 +8,23 @@ await page.goto('https://books.toscrape.com/')
 
 // want to extract (img link, ratings, title, price, inStock, link
 const data = await page.evaluate(() => {
-      const bookElements = document?.querySelectorAll('.product_pod');
-      const arr = Array?.from(bookElements)
-      const imgLink = arr?.map(e => e?.querySelector('a img.thumbnail')?.getAttribute('src'))
-      const title = arr?.map(e => e?.querySelector('h3 a')?.getAttribute('title'))
-      const ratings = arr?.map(e => e?.querySelector('p.star-rating')?.className?.split(' ')[1])
-      const price = arr?.map(e => e?.querySelector('p.price_color')?.textContent)
-      const inStock = arr?.map(e => e?.querySelector('p.price_color')?.textContent ? true : false)
-      const productLink = arr?.map(e => e?.querySelector('.image_container a')?.getAttribute('href'))
+      return books = Array.from(document.querySelectorAll('.product_pod'), (book)=> {
+            const imgLink = book?.querySelector('a img.thumbnail')?.getAttribute('src')
+            const title = book?.querySelector('h3 a')?.getAttribute('title')
+            const ratings = book?.querySelector('p.star-rating')?.className?.split(' ')[1]
+            const price = book?.querySelector('p.price_color')?.textContent
+            const inStock = book?.querySelector('p.price_color')?.textContent ? true : false
+            const productLink = book?.querySelector('.image_container a')?.getAttribute('href')
 
-      return productLink
+            return{
+                  imgLink,
+                  title,
+                  ratings,
+                  price,
+                  inStock,
+                  productLink
+            }
+      });
 }
 )
 
