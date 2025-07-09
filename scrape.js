67845ddbe8d10befe2fb5fe5fd8f909ef204460a
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-07-09 15:05:45 +0500

    got data from required no of pages

--- scrape.js
+++ scrape.js
@@ -1,31 +1,42 @@
 import puppeteer from "puppeteer";
 
+const url = `https://books.toscrape.com/catalogue/page-$.html`
+
 const browser = await puppeteer.launch()
 const page = await browser.newPage()
 
-await page.goto('https://books.toscrape.com/')
+const allBooks = []
+let currentPg = 1
+const maxPg = 5
 
+while(currentPg <= maxPg){
+      await page.goto(`https://books.toscrape.com/catalogue/page-${currentPg}.html`)
 
-// want to extract (img link, ratings, title, price, inStock, link
-const data = await page.evaluate(() => {
-      return books = Array.from(document.querySelectorAll('.product_pod'), (book)=> {
-            const imgLink = book?.querySelector('a img.thumbnail')?.getAttribute('src')
-            const title = book?.querySelector('h3 a')?.getAttribute('title')
-            const ratings = book?.querySelector('p.star-rating')?.className?.split(' ')[1]
-            const price = book?.querySelector('p.price_color')?.textContent
-            const inStock = book?.querySelector('p.price_color')?.textContent ? true : false
-            const productLink = book?.querySelector('.image_container a')?.getAttribute('href')
+      // want to extract (img link, ratings, title, price, inStock, link
+      const data = await page.evaluate(() => {
+            return books = Array.from(document.querySelectorAll('.product_pod'), (book)=> {
+                  const imgLink = book?.querySelector('a img.thumbnail')?.getAttribute('src')
+                  const title = book?.querySelector('h3 a')?.getAttribute('title')
+                  const ratings = book?.querySelector('p.star-rating')?.className?.split(' ')[1]
+                  const price = book?.querySelector('p.price_color')?.textContent
+                  const inStock = book?.querySelector('p.price_color')?.textContent ? true : false
+                  const productLink = book?.querySelector('.image_container a')?.getAttribute('href')
+            
+                  return{
+                              imgLink,
+                              title,
+                              ratings,
+                              price,
+                              inStock,
+                              productLink
+                        }
 
-            return{
-                  imgLink,
-                  title,
-                  ratings,
-                  price,
-                  inStock,
-                  productLink
+                  });
             }
-      });
+      )
+      allBooks.push(...data)
+      console.log(`Data of pg ${currentPg}`, data)
+      currentPg++
 }
-)
 
-console.log(data, data?.length)
+await browser.close();
