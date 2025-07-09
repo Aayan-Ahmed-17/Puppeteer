476263a0bd572932d327b18b8a8b0d30baf76ac4
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-07-09 11:20:46 +0500

    got img link

--- scrape.js
+++ scrape.js
@@ -5,11 +5,15 @@ const page = await browser.newPage()
 
 await page.goto('https://books.toscrape.com/')
 
+
+// want to extract (img link, ratings, title, price, inStock, link
 const data = await page.evaluate(() => {
       const bookElements = document.querySelectorAll('.product_pod');
       const arr = Array.from(bookElements)
+      const imgLink = arr.map(e => e.querySelector('a img.thumbnail').getAttribute('src'))
       const title = arr.map(e => e.querySelector('h3 a').getAttribute('title'))
-      return title
+      // const 
+      return imgLink
 })
 
 console.log(data)
