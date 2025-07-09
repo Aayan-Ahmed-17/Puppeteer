0df68be361cf6d9ea736eb3e070b6a5701e7ef52
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-07-09 11:53:11 +0500

    got ratings, price, inStock, link | opt chain implemented

--- scrape.js
+++ scrape.js
@@ -8,12 +8,17 @@ await page.goto('https://books.toscrape.com/')
 
 // want to extract (img link, ratings, title, price, inStock, link
 const data = await page.evaluate(() => {
-      const bookElements = document.querySelectorAll('.product_pod');
-      const arr = Array.from(bookElements)
-      const imgLink = arr.map(e => e.querySelector('a img.thumbnail').getAttribute('src'))
-      const title = arr.map(e => e.querySelector('h3 a').getAttribute('title'))
-      // const 
-      return imgLink
-})
+      const bookElements = document?.querySelectorAll('.product_pod');
+      const arr = Array?.from(bookElements)
+      const imgLink = arr?.map(e => e?.querySelector('a img.thumbnail')?.getAttribute('src'))
+      const title = arr?.map(e => e?.querySelector('h3 a')?.getAttribute('title'))
+      const ratings = arr?.map(e => e?.querySelector('p.star-rating')?.className?.split(' ')[1])
+      const price = arr?.map(e => e?.querySelector('p.price_color')?.textContent)
+      const inStock = arr?.map(e => e?.querySelector('p.price_color')?.textContent ? true : false)
+      const productLink = arr?.map(e => e?.querySelector('.image_container a')?.getAttribute('href'))
 
-console.log(data)
+      return productLink
+}
+)
+
+console.log(data, data?.length)
